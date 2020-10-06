13c769bb04aa1bf69855f67db3fd20d979edb9c2
Tim Ruesink <timothy.ruesink@gmail.com>
2020-10-05 19:52:21 -0500

    general progress

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+// import Google important keys
+const keys = require("./config/keys");
+
 const app = express();
 
-//route handler
-app.get('/', (req, res) => {
-  res.send('hi there');
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
 // logic for PORT choice
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('server running on port 5000');
+  console.log("server running on port 5000");
 });
