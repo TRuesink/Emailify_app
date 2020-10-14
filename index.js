1762bfb2993e3aa0e0b96bd97beca2a2dd83efdd
Tim Ruesink <timothy.ruesink@gmail.com>
2020-10-14 18:48:25 -0500

    added heroku client build requirements

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve production assets like our main.js file or main.css file
+  app.use(express.static("client/build"));
+  // Express will serve the index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // logic for PORT choice
 const PORT = process.env.PORT || 5000;
 
